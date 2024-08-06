e515030fb52f74fc79354e6c9482d65b1d0440ac
Krish Bhalala <145704986+Krish-Bhalala@users.noreply.github.com>
2024-08-06 00:12:41 -0500

    redesgined the color theme and UI, enabled description texts on click

--- public/resources/resources.js
+++ public/resources/resources.js
@@ -1,2 +1,27 @@
-const titles = Array.from(document.querySelectorAll("titles"));
+const titles = Array.from(document.querySelectorAll(".resources"));
+const descriptions = Array.from(document.querySelectorAll(".description-text"));
+console.log(titles);
+titles.forEach((element) => {
+    element.addEventListener("click",displayContext);
+    
+});
 
+function displayContext(){
+    const descriptionTextNode = this.querySelector('.description-text');
+    if (descriptionTextNode && descriptionTextNode.classList.contains('description-text')) {
+        toggleDisplay(descriptionTextNode);
+    } else {
+        console.log('No description-text found immediately after this title');
+    }
+}
+
+function toggleDisplay(element) {
+    const computedStyle = window.getComputedStyle(element);
+    if (computedStyle.display === "none") {
+        element.style.display = "block";
+        element.parentNode.classList.add("open");
+    } else {
+        element.style.display = "none";
+        element.parentNode.classList.remove("open");
+    }
+}
