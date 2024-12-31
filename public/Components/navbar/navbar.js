a5c8f5d82afb71243fc7e6ee38c612fa9d8b99e6
Krish Bhalala <145704986+Krish-Bhalala@users.noreply.github.com>
2024-12-31 00:41:37 -0600

    updated logo design for better visibility and updated the respective navbars

--- public/Components/navbar/navbar.js
+++ public/Components/navbar/navbar.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     navbarContainer.innerHTML = `
         <div class="navbar">
             <div class="navbar__logo">
-                <img src="../public/club Assets/activeMindsLogo.png" alt="logo"> 
+                <img src="../public/club Assets/activeMindLogoOutlined.png" alt="logo"> 
             </div>
             <div class="menu-container">
                 <div id="menu-bar">
