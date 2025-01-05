c6c3f43d42de3684cbb42e57b015ef322c93cc01
RamatjyotSingh <ramatjyot13.ca@gmail.com>
2025-01-04 23:59:02 -0600

    website deployed @ activeminds-umanitoba.org

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: './', // Use relative paths for assets
+  build: {
+    outDir: 'dist', // Ensure the output directory matches your desired structure
+    rollupOptions: {
+        input: {
+            main: './public/index.html',  // Main entry point
+            events: './public/events.html',  // Events page
+            resources: './public/resources.html', // Resources page
+            teams: './public/Teams.html',  // Teams page
+          },
+    },
+  },
+});
