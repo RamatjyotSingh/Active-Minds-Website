1c781027f6eeb626ae6ea84645f2a6139770440e
RamatjyotSingh <ramatjyot13.ca@gmail.com>
2025-01-04 20:53:06 -0600

    minor changes

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default daefineConfig({
+  root: './',  // Specify the project root directory
+  build: {
+    outDir: 'dist', // Output directory for the build files
+    minify: true,  // Enable minification for production
+    rollupOptions: {
+      input: {
+        main: './public/index.html',
+        events: './public/events.html',
+        resources: './public/resources.html',
+        teams: './public/teams.html',
+       
+      }
+    }
+  }
+});
